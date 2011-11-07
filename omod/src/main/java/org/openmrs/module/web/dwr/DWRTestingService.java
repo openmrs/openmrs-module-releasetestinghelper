9dd630c2c84841159886d78a9e23bbb45e5d3527
Wyclif Luyima <wyclif@openmrs.org>
2011-11-07 19:12:14 +0000

    Add a DWRAuthenticationService to test module - TRUNK-2834

---
+++ omod/src/main/java/org/openmrs/module/web/dwr/DWRTestingService.java
@@ -0,0 +1,45 @@
+package org.openmrs.module.web.dwr;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.ContextAuthenticationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Contains methods that process DWR calls for the module.
+ */
+@Controller
+public class DWRTestingService {
+	
+	private static final Log log = LogFactory.getLog(DWRTestingService.class);
+	
+	/**
+	 * Processes user authentication
+	 * 
+	 * @param username the username of the user
+	 * @param password the password of the user
+	 * @return true if the user is successfully authenticated otherwise false
+	 */
+	public boolean authenticateAsSuperUser(@RequestParam(value = "username") String username,
+	                                       @RequestParam(value = "password") String password) {
+		if (log.isDebugEnabled())
+			log.debug("Authenticating user...");
+		
+		try {
+			Context.authenticate(username, password);
+			if (Context.isAuthenticated() && Context.getAuthenticatedUser().isSuperUser()) {
+				if (log.isDebugEnabled())
+					log.debug("Authenticated successfully...");
+				return true;
+			}
+		}
+		catch (ContextAuthenticationException e) {
+			//do nothing
+		}
+		
+		return false;
+	}
+	
+}
